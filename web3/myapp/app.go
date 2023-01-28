150cc238751b2b55f6831ce80ff09a37dc8ae9de
hoondal69 <davidtwins6970@gmail.com>
2023-01-28 17:33:57 +0900

    vars()로 파싱기능 구현

--- web3/myapp/app.go
+++ web3/myapp/app.go
@@ -3,6 +3,8 @@ package myapp
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
@@ -13,12 +15,17 @@ func usersHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Get UserInfo by /users/{id}")
 }
 
+func getUserInfoHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	fmt.Fprint(w, "User ID:", vars["id"])
+}
+
 // NewHandler make a new myapp handler
 func NewHandler() http.Handler {
-	mux := http.NewServeMux()
+	mux := mux.NewRouter()
 
 	mux.HandleFunc("/", indexHandler)
-	mux.HandleFunc("/users", usersHandler)
-
+	mux.HandleFunc("/users", usersHandler) //test를 처음에 '/users/test'로 요청을 보내도 테스트가 성공하는데, 이는 요청이 없는 경우에 자동으로 그 상위 요청하기 때문.
+	mux.HandleFunc("/users/{id:[0-9]+}", getUserInfoHandler)
 	return mux
 }
--- web3/myapp/app_test.go
+++ web3/myapp/app_test.go
@@ -23,3 +23,17 @@ func TestIndex(t *testing.T) {
 	data, _ := ioutil.ReadAll(resp.Body)
 	assert.Contains(string(data), "Get UserInfo")
 }
+
+func TestGetUserInfo(t *testing.T) {
+	assert := assert.New(t)
+
+	ts := httptest.NewServer(NewHandler()) //새로운 서버를 만드는 거다. 서버에서는 새로운 핸들러를 집어넣어야 한다.
+	//근데 실제 http서버가 아니고, test 하기위한 서버를 생성한것. 그리고 항상 닫아줘야함
+	defer ts.Close()
+
+	resp, err := http.Get(ts.URL + "/users/89")
+	assert.NoError(err)
+	assert.Equal(http.StatusOK, resp.StatusCode)
+	data, _ := ioutil.ReadAll(resp.Body)
+	assert.Contains(string(data), "User ID:89")
+}
